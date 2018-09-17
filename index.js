5340d06318493c793d55c949e5087d70d6bfd18a
Julian Krenge <julian@parcellab.com>
2018-09-17 11:45:15 +0200

    💣 Initial version

---
+++ index.js
@@ -0,0 +1,55 @@
+// dependencies
+
+const _ = require('underscore');
+const csv = require('fast-csv');
+const fs = require('fs');
+
+const express = require('express');
+const methodOverride = require('method-override');
+const bodyParser = require('body-parser');
+
+// disk operations
+
+var cache = [];
+
+function writeCacheToFile() {
+  var filename = `tickets_${(new Date()).toJSON()}.csv`;
+  console.log('Writing to ' + `tickets_${(new Date()).toJSON()}.csv`);
+
+  var csvStream = csv.format({ headers: true, delimiter: ';' })
+  var writableStream = fs.createWriteStream(filename);
+
+  writableStream.on('finish', function () {
+    cache = []; // reset
+    setTimeout(writeCacheToFile, 30000); // recursion
+    console.log('All written to ' + filename);
+  });
+
+  csvStream.pipe(writableStream);
+  cache.forEach(c => csvStream.write(c));
+  csvStream.end();
+
+}
+
+writeCacheToFile();
+
+// web server to receive webhooks
+
+var app = express();
+app.use(methodOverride('X-HTTP-Method-Override'));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+app.get('/ping', (req, res) => res.status(200).send('pong'));
+
+app.post('/webhook', function (req, res) {
+  
+  var payload = req.body;
+  console.log('Received: ' + JSON.stringify(payload));
+  cache.push(payload);
+
+  res.status(202).send('ack');
+
+});
+
+app.listen(8080);
